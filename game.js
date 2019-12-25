241518b5cfa959c37ea2a5eb3d935c0753e17acf
Donald Smith <dsmith94@gmail.com>
2019-12-24 21:20:19 -0800

    Allow preface text

--- game.js
+++ game.js
@@ -23,7 +23,6 @@ class Game extends CoreGameClass {
 
         this.currentRoom = this.map.get('outsideOffice');
         
-        /*
         this.prefaceText = applyEmphasis(`<center><b>July 3, 1922</b></center>
         ~~It's hot as the flames of Perdition and you got a job to do. Not your usual 9 to 5 as secretary
         for that fat dumb jerk, Mr. Gibbs. Your <i>real</i> gig, which amounts to scuttling scumballs off this mortal coil.
@@ -31,7 +30,6 @@ class Game extends CoreGameClass {
         next train to that big speakeasy in the sky. You've known it for a while, but the guy you've worked a typewriter for
         over the last six months is a traitor and a spy.
         ~~Easy peasy cakewalk. You're a secret agent, and just the dame for the job.`);
-        */
         
     }
 

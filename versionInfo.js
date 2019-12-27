08e592c918f08d56259704b7281e8775e3219caf
Donald Smith <dsmith94@gmail.com>
2019-12-26 23:50:18 -0800

    fixed bottom margining

---
+++ versionInfo.js
@@ -0,0 +1,12 @@
+
+const versionInfo = {
+    IFID: '6F089461-A65E-4C94-BFBE-ECD58799096E',
+    title: 'Zork Fiction',
+    byline: 'by Mr I Want Rye',
+    tags: ['zorkian,', 'fantasy', 'comic', 'comedy'],
+    version: '0.1a',
+    authorEmail: 'iwantrye <iwantrye@yahoo.com>',
+    desc: `I found this manuscript in a forgotten corner of an old abbey. It claims to be a lost chapter from 
+           the history of the Eastlands. After careful study, however, I doubt it to be authentic. Surely, this is only a work
+           of Zork Fiction.`,
+};
